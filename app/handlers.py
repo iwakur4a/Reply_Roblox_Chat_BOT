f28d2d787fdd312d75cf1cb8682e465ba699841e
iwakur4a <iwakura.code@gmail.com>
2025-10-29 22:36:34 +0200

    work

--- app/handlers.py
+++ app/handlers.py
@@ -1,34 +1,33 @@
-from random import choice
+import datetime
 from aiogram import F, Router
 from aiogram.filters import Command
 from aiogram.types import Message
-import datetime
-
-now = datetime.datetime.now()
 
 
 router = Router()
 
 
-@router.message(F.text)
-async def apfs(message: Message):
-    if now.hour == 15 and now.minute == 22:
-        await message.answer("11:11")
+@router.message(Command('kto_ti'))
+async def get_photo(message: Message):
+    await message.answer_photo(
+        photo='https://i.ebayimg.com/images/g/rdQAAOSwzlhnMDIC/s-l1600.jpg',
+        caption='эт я))')
 
 
-@router.message(F.text == "брейнрот" or F.text == "Брейнрот")  # сверка текста юзера, если совпадает
-async def brainrot(message: Message):  # отве
+@router.message(F.text.lower() == "брейнрот")
+async def brainrot(message: Message):
     await message.answer("тунг тунг тунг сахууур")
 
 
-@router.message(F.text == "тралалело" or F.text == "Тралалело")
+@router.message(F.text.lower() == "тралалело")
 async def tralalelo(message: Message):
     await message.answer_photo(
         photo='https://upload.wikimedia.org/wikipedia/commons/f/f6/Tralalero_Tralala.webp',
         caption="тралала")
 
 
-@router.message(Command('kto_ti'))
-async def get_photo(message: Message):
-    await message.answer_photo(photo='https://i.ebayimg.com/images/g/rdQAAOSwzlhnMDIC/s-l1600.jpg',
-                               caption='эт я))')
+@router.message(F.text)  # Этот хендлер ПОСЛЕДНИМ, чтобы не блокировал остальные
+async def check_time(message: Message):
+    now = datetime.datetime.now()
+    if now.hour == 22 and now.minute == 34:
+        await message.bot.send_message(chat_id=-1003220158532, text="11:11")
